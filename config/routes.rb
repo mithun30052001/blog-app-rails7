19c97d2b5d3da03f09e8bc24c203a785b02de08f
mithun30052001 <lmithun01@gmail.com>
2023-12-05 13:59:47 +0530

    user name added to users table under devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   resources :posts
   get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
